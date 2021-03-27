4a3526f0d89f15cedcdbaac38e704e4b74f3924f
Acsolobalt <78843052+Acsolobalt@users.noreply.github.com>
2021-03-27 19:46:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,25 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
- int count = 0;
-  if (arr[size / 2] == value) {
-   int count1 = 0, count2 = 0;
-    do {
-     ++count1;
-    } while (arr[size / 2 + count1] == value);
-    do {
-     ++count2;
-    } while (arr[size / 2 - count2] == value);
-    return count1 + count2 - 1;
-  } else {
-   if (arr[size / 2] > value) {
-    return cbinsearch(arr, size / 2, value);
-   } else {
-    return cbinsearch(arr + size / 2 + 1, size / 2, value);
-   }
-  }
-  return 0; // если ничего не найдено
+int count = 0;
+	int middle = size / 2;
+	if (!size) { return 0; }
+	if (arr[middle] == value) {
+		int count1 = 0, count2 = 0;
+		do {
+			++count1;
+		} while (arr[middle + count1] == value);
+		do {
+			++count2;
+		} while (arr[middle - count2] == value);
+		return count1 + count2 - 1;
+	}
+	else {
+		if (arr[middle] > value) {
+			return cbinsearch(arr, middle, value);
+		}
+		else {
+			return cbinsearch(arr + middle + 1, middle, value);
+		}
+	}
 }
